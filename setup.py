5e9150ec9e1e99cc4630ed364195a54afbdbf66e
Andrew Cole <andrew.cole@illallangi.com>
2020-08-31 12:39:28 +1000

    E261

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="flare-updatr-illallangi", # Replace with your own username
+    name="flare-updatr-illallangi",
     version="0.0.1",
     author="Andrew Cole",
     author_email="andrew.cole@illallangi.com",
